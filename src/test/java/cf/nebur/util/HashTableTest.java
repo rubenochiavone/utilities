cacfe3c7ca0c3a10e3ad16a69f7e7fc5dd744a55
Ruben O. Chiavone <ruben.ochiavone@gmail.com>
2018-01-14 23:25:40 -0300

    Improved testing

--- src/test/java/cf/nebur/util/HashTableTest.java
+++ src/test/java/cf/nebur/util/HashTableTest.java
@@ -34,10 +34,7 @@ public class HashTableTest extends TestCase {
         HashTable<String, Integer> stringIntegerHashTable =
                 new HashTable<>();
 
-        int i;
-
-        for (i = 0; i < length; i++) {
-            String key = words[i];
+        for (String key : words) {
             int value = 0;
 
             for (char c : key.toCharArray()) {
@@ -45,7 +42,160 @@ public class HashTableTest extends TestCase {
             }
 
             stringIntegerHashTable.put(key, value);
+        }
 
+        assertEquals(length, stringIntegerHashTable.size());
+    }
+
+    @Test
+    public void testBigStringIntegerPutWithInitialCapacity() {
+        String aVeryLongString = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+        String[] words = aVeryLongString.split(" ");
+        int length = words.length;
+
+        HashTable<String, Integer> stringIntegerHashTable =
+                new HashTable<>(91);
+
+        for (String key : words) {
+            int value = 0;
+
+            for (char c : key.toCharArray()) {
+                value += c;
+            }
+
+            stringIntegerHashTable.put(key, value);
+        }
+
+        assertEquals(length, stringIntegerHashTable.size());
+    }
+
+    @Test
+    public void testBigStringIntegerPutWithLoadFactor() {
+        String aVeryLongString = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+        String[] words = aVeryLongString.split(" ");
+        int length = words.length;
+
+        HashTable<String, Integer> stringIntegerHashTable =
+                new HashTable<>(1.0f);
+
+        for (String key : words) {
+            int value = 0;
+
+            for (char c : key.toCharArray()) {
+                value += c;
+            }
+
+            stringIntegerHashTable.put(key, value);
+        }
+
+        assertEquals(length, stringIntegerHashTable.size());
+    }
+
+    @Test
+    public void testBigStringIntegerPutWithInitialCapacityLoadFactor() {
+        String aVeryLongString = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+        String[] words = aVeryLongString.split(" ");
+        int length = words.length;
+
+        HashTable<String, Integer> stringIntegerHashTable =
+                new HashTable<>(91,1.0f);
+
+        for (String key : words) {
+            int value = 0;
+
+            for (char c : key.toCharArray()) {
+                value += c;
+            }
+
+            stringIntegerHashTable.put(key, value);
+        }
+
+        assertEquals(length, stringIntegerHashTable.size());
+    }
+
+    @Test
+    public void testBiggerStringIntegerPut() {
+        String aVeryLongString = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
+        String[] words = aVeryLongString.split(" ");
+        int length = words.length;
+
+        HashTable<String, Integer> stringIntegerHashTable =
+                new HashTable<>();
+
+        for (String key : words) {
+            int value = 0;
+
+            for (char c : key.toCharArray()) {
+                value += c;
+            }
+
+            stringIntegerHashTable.put(key, value);
+        }
+
+        assertEquals(length, stringIntegerHashTable.size());
+    }
+
+    @Test
+    public void testBiggerStringIntegerPutWithInitialCapacity() {
+        String aVeryLongString = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
+        String[] words = aVeryLongString.split(" ");
+        int length = words.length;
+
+        HashTable<String, Integer> stringIntegerHashTable =
+                new HashTable<>(91);
+
+        for (String key : words) {
+            int value = 0;
+
+            for (char c : key.toCharArray()) {
+                value += c;
+            }
+
+            stringIntegerHashTable.put(key, value);
+        }
+
+        assertEquals(length, stringIntegerHashTable.size());
+    }
+
+    @Test
+    public void testBiggerStringIntegerPutWithLoadFactor() {
+        String aVeryLongString = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
+        String[] words = aVeryLongString.split(" ");
+        int length = words.length;
+
+        HashTable<String, Integer> stringIntegerHashTable =
+                new HashTable<>(1.0f);
+
+        for (String key : words) {
+            int value = 0;
+
+            for (char c : key.toCharArray()) {
+                value += c;
+            }
+
+            stringIntegerHashTable.put(key, value);
+        }
+
+        assertEquals(length, stringIntegerHashTable.size());
+    }
+
+    @Test
+    public void testBiggerStringIntegerPutWithInitialCapacityLoadFactor() {
+        String aVeryLongString = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
+        String[] words = aVeryLongString.split(" ");
+        int length = words.length;
+
+        HashTable<String, Integer> stringIntegerHashTable =
+                new HashTable<>(91,1.0f);
+
+        for (String key : words) {
+            int value = 0;
+
+            for (char c : key.toCharArray()) {
+                value += c;
+            }
+
+            stringIntegerHashTable.put(key, value);
         }
 
         assertEquals(length, stringIntegerHashTable.size());
