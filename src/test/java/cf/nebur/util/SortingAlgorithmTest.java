810376bebfea58b4eedc16dc277382c6394106d2
Ruben O. Chiavone <ruben.ochiavone@gmail.com>
2018-01-16 21:41:59 -0300

    Updated based on SortingAlgorithms changes
    Improved testing

--- src/test/java/cf/nebur/util/SortingAlgorithmTest.java
+++ src/test/java/cf/nebur/util/SortingAlgorithmTest.java
@@ -7,8 +7,22 @@ public class SortingAlgorithmTest extends TestCase {
 
     @Test
     public void testIntegerMergeSort() {
+        testIntegerGenericSort(SortingAlgorithm.MERGE_SORT);
+    }
+
+    @Test
+    public void testIntegerInsertionSort() {
+        testIntegerGenericSort(SortingAlgorithm.INSERTION_SORT);
+    }
+
+    @Test
+    public void testIntegerBubbleSort() {
+        testIntegerGenericSort(SortingAlgorithm.BUBBLE_SORT);
+    }
+
+    public void testIntegerGenericSort(SortingAlgorithm algorithm) {
         Sortable<Integer> integerSortable =
-                SortableFactory.create(Integer.class, SortingAlgorithm.MERGE_SORT);
+                SortableFactory.create(Integer.class, algorithm);
 
         assertNotNull(integerSortable);
 
@@ -29,7 +43,9 @@ public class SortingAlgorithmTest extends TestCase {
         }
 
         integerSortable =
-                SortableFactory.create(Integer.class, SortingAlgorithm.MERGE_SORT_DESC);
+                SortableFactory.create(Integer.class,
+                        algorithm,
+                        SortingAlgorithm.Order.DESC);
 
         assertNotNull(integerSortable);
 
@@ -40,7 +56,7 @@ public class SortingAlgorithmTest extends TestCase {
         }
 
         integerSortable =
-                SortableFactory.create(Integer.class, SortingAlgorithm.MERGE_SORT);
+                SortableFactory.create(Integer.class, algorithm);
 
         assertNotNull(integerSortable);
 
@@ -57,54 +73,52 @@ public class SortingAlgorithmTest extends TestCase {
             assertEquals(i, sortedArray[i - 1].intValue());
         }
 
+        integerSortable =
+                SortableFactory.create(Integer.class, algorithm);
+
+        assertNotNull(integerSortable);
+
+        array = new Integer[6];
+        array[0] = 8;
+        array[1] = 2;
+        array[2] = 4;
+        array[3] = 9;
+        array[4] = 3;
+        array[5] = 6;
+
+        sortedArray = integerSortable.sort(array);
+
+        assertEquals(2, sortedArray[0].intValue());
+        assertEquals(3, sortedArray[1].intValue());
+        assertEquals(4, sortedArray[2].intValue());
+        assertEquals(6, sortedArray[3].intValue());
+        assertEquals(8, sortedArray[4].intValue());
+        assertEquals(9, sortedArray[5].intValue());
+
+        integerSortable =
+                SortableFactory.create(Integer.class,
+                        algorithm,
+                        SortingAlgorithm.Order.DESC);
+
+        assertNotNull(integerSortable);
+
+        array = new Integer[6];
+        array[0] = 8;
+        array[1] = 2;
+        array[2] = 4;
+        array[3] = 9;
+        array[4] = 3;
+        array[5] = 6;
+
+        sortedArray = integerSortable.sort(array);
+
+        assertEquals(9, sortedArray[0].intValue());
+        assertEquals(8, sortedArray[1].intValue());
+        assertEquals(6, sortedArray[2].intValue());
+        assertEquals(4, sortedArray[3].intValue());
+        assertEquals(3, sortedArray[4].intValue());
+        assertEquals(2, sortedArray[5].intValue());
+
         // TODO: improve testing
-    }
-
-    @Test
-    public void testIntegerInsertionSort() {
-        Sortable<Integer> integerSortable =
-                SortableFactory.create(Integer.class, SortingAlgorithm.INSERTION_SORT);
-
-        assertNotNull(integerSortable);
-
-        Integer[] array = new Integer[8];
-        array[0] = 5;
-        array[1] = 3;
-        array[2] = 1;
-        array[3] = 6;
-        array[4] = 7;
-        array[5] = 2;
-        array[6] = 8;
-        array[7] = 4;
-
-        Integer[] sortedArray = integerSortable.sort(array);
-
-        for (int i = 1; i <= 8; i++) {
-            assertEquals(i, sortedArray[i - 1].intValue());
-        }
-    }
-
-    @Test
-    public void testIntegerBubbleSort() {
-        Sortable<Integer> integerSortable =
-                SortableFactory.create(Integer.class, SortingAlgorithm.BUBBLE_SORT);
-
-        assertNotNull(integerSortable);
-
-        Integer[] array = new Integer[8];
-        array[0] = 5;
-        array[1] = 3;
-        array[2] = 1;
-        array[3] = 6;
-        array[4] = 7;
-        array[5] = 2;
-        array[6] = 8;
-        array[7] = 4;
-
-        Integer[] sortedArray = integerSortable.sort(array);
-
-        for (int i = 1; i <= 8; i++) {
-            assertEquals(i, sortedArray[i - 1].intValue());
-        }
     }
 }
